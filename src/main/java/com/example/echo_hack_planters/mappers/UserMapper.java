fa4f1644d12d0b461720799a7bf8aa1b683d2ee9
Alushi, Sonja <sonja.alushi@lhind.dlh.de>
2023-05-06 04:08:11 +0200

    Adding mappers

--- src/main/java/com/example/echo_hack_planters/mappers/UserMapper.java
+++ src/main/java/com/example/echo_hack_planters/mappers/UserMapper.java
@@ -1,4 +1,18 @@
 package com.example.echo_hack_planters.mappers;
 
-public interface UserMapper {
+import com.example.echo_hack_planters.model.User;
+import com.example.echo_hack_planters.model.UserApi;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "cdi")
+public interface UserMapper  {
+  @Mapping(target = "password", ignore = true)
+  @Mapping(target = "name", source = "name")
+  User intoEntity(UserApi user);
+
+  @Mapping(target = "name", source = "name")
+  UserApi fromEntity(User entity);
+
+
 }
